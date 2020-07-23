2a2ff18e2fe016592c2810f0f30f53ab05fedbee
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2020-07-23 23:04:11 +0300

    Created clients

---
+++ Clients/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clients.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult One()
+        {
+            return View();
+        }
+        public IActionResult Two()
+        {
+            return View();
+        }
+        public IActionResult Three()
+        {
+            return View();
+        }
+    }
+}
--- Clients/Startup.cs
+++ Clients/Startup.cs
@@ -1,19 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
-using DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
-namespace MessageService
+namespace Clients
 {
     public class Startup
     {
@@ -27,19 +23,7 @@ namespace MessageService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            var connectionString = Configuration.GetSection("DbConnection").Value;
-
-            services.AddSingleton<IMessagesRepository>(new PostgresqlRepository(connectionString));
-
-            services.AddCors();
-
-            services.AddControllers();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Version = "V1", Title = "Messages API"});
-            });
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,11 +33,14 @@ namespace MessageService
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseCors(builder =>
-                builder.AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod());
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -61,14 +48,9 @@ namespace MessageService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-            });
-
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Messages API V1");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
